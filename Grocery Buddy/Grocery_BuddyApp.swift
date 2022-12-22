ed5a023ab8cee1c03834b203af0b868518c7b526
Michael Sebsbe <Michael.mulugeta2010@gmail.com>
2022-12-21 21:03:48 -0500

    Initial Commit

---
+++ Grocery Buddy/Grocery_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_BuddyApp.swift
+//  Grocery Buddy
+//
+//  Created by Michael Sebsbe on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
